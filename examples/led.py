543774c9d366165252a0f92a21ddfa0d34e07241
Maksym Borodin <maksym_b@orsoc.se>
2013-12-18 09:57:39 +0100

    Added examples

---
+++ examples/led.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from knc import led
+
+import time
+
+
+def on_off_red_led():
+    red = led.get_led('red')
+    red.on()
+    time.sleep(30)
+    red.off()
+
+
+def blink_rgb():
+    rgb = led.get_led('rgb')
+    rgb.set_mode(led.LED_MODE_BLINKING)
+    rgb.on()
+    time.sleep(30)
+    rgb.off()
+
+
+def blink_leds():
+    l = led.get_led('green')
+    l.set_mode(led.LED_MODE_BLINKING)
+    l.frequency = 5
+    l.on()
+
+    l = led.get_led('rgb')
+    l.set_mode(led.LED_MODE_BLINKING)
+    l.frequency = 10
+    l.on()
+
+    time.sleep(15)
+    l.frequency = 20
+
+    time.sleep(15)
+    led.shutdown_blinking_leds()
+
+
+if __name__ == '__main__':
+    on_off_red_led()
+    blink_rgb()
+    blink_leds()
